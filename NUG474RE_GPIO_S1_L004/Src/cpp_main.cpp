6bedae8d9fc62d1b5ea5abb9d56d3a953acf0dc9
eugen.lin <eugen.lin@raritan.com>
2025-05-26 17:07:20 +0800

    2025May26 1. To update GPIO_S1_L004 project.

---
+++ NUG474RE_GPIO_S1_L004/Src/cpp_main.cpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+
+// Register access encapsulated in structs
+struct GPIOA_Registers {
+    static volatile uint32_t& MODER()   { return *(volatile uint32_t*)0x48000000UL; }
+    static volatile uint32_t& OTYPER()  { return *(volatile uint32_t*)0x48000004UL; }
+    static volatile uint32_t& ODR()     { return *(volatile uint32_t*)0x48000014UL; }
+};
+
+struct RCC_Registers {
+    static volatile uint32_t& AHB2ENR() { return *(volatile uint32_t*)0x4002104CUL; }
+};
+
+constexpr uint32_t PIN5 = (1U << 5);
+
+inline void simple_delay(volatile uint32_t t) {
+    while (t--);
+}
+
+int main() {
+    RCC_Registers::AHB2ENR() |= (1U << 0);
+    (void)RCC_Registers::AHB2ENR();
+
+    GPIOA_Registers::MODER() &= ~(3U << (5 * 2));
+    GPIOA_Registers::MODER() |=  (1U << (5 * 2));
+    GPIOA_Registers::OTYPER() &= ~(1U << 5);
+
+    while (1) {
+        GPIOA_Registers::ODR() &= ~PIN5;
+        simple_delay(800000);
+        GPIOA_Registers::ODR() |= PIN5;
+        simple_delay(800000);
+    }
+}
